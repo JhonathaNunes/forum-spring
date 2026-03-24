f9503f7b4516e684486872fa0a5d6a89263d0980
JhonathaNunes <jhonathasilveira@gmail.com>
2026-03-24 15:24:51 -0300

    Install jpa and define entities

--- src/main/kotlin/br/com/jhonatha/forum/models/Answer.kt
+++ src/main/kotlin/br/com/jhonatha/forum/models/Answer.kt
@@ -1,13 +1,22 @@
 package br.com.jhonatha.forum.models
 
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
 import java.time.LocalDateTime
 
+@Entity
 data class Answer(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
     val message: String,
     val createdAt: LocalDateTime = LocalDateTime.now(),
     val updatedAt: LocalDateTime = LocalDateTime.now(),
+    @ManyToOne
     val author: User,
+    @ManyToOne
     val topic: Topic,
     val isSolution: Boolean = false
 )
--- src/main/kotlin/br/com/jhonatha/forum/models/Topic.kt
+++ src/main/kotlin/br/com/jhonatha/forum/models/Topic.kt
@@ -1,15 +1,31 @@
 package br.com.jhonatha.forum.models
 
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToMany
+import org.hibernate.annotations.UpdateTimestamp
 import java.time.LocalDateTime
 
+@Entity
 data class Topic (
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
     val title: String,
     val message: String,
     val createdAt: LocalDateTime = LocalDateTime.now(),
+    @UpdateTimestamp
     val updatedAt: LocalDateTime = LocalDateTime.now(),
+    @ManyToOne
     val course: Course,
+    @ManyToOne
     val author: User,
+    @Enumerated(EnumType.STRING)
     val status: TopicStatus = TopicStatus.NOT_ANSWERED,
-    val answers: List<Answer> = ArrayList(),
+    @OneToMany(mappedBy = "topic")
+    val answers: MutableList<Answer> = ArrayList(),
 )

649344452efc92b1b1df5029af730f3161fd67fd
JhonathaNunes <jhonathasilveira@gmail.com>
2026-05-11 13:32:18 -0300

    Implement answer repository

--- src/main/kotlin/br/com/jhonatha/forum/services/AnswerService.kt
+++ src/main/kotlin/br/com/jhonatha/forum/services/AnswerService.kt
@@ -4,56 +4,44 @@ import br.com.jhonatha.forum.dto.NewAnswerRequest
 import br.com.jhonatha.forum.dto.UpdateAnswerRequest
 import br.com.jhonatha.forum.exceptions.NotFoundException
 import br.com.jhonatha.forum.models.Answer
+import br.com.jhonatha.forum.repositories.AnswerRepository
 import org.springframework.stereotype.Service
 
 @Service
 class AnswerService(
-    private var answers: List<Answer>,
+    private val repository: AnswerRepository,
     private val userService: UserService,
     private val topicService: TopicService,
     private val notFoundErrorMessage: String = "Answer with id %d not found",
 ) {
 
     fun getAnswersFromTopic(topicId: Long): List<Answer> {
-        return answers.filter {
-            it.topic.id == topicId
-        }
+        return repository.findByTopicId(topicId)
     }
 
     fun create(id: Long, request: NewAnswerRequest): Answer {
         val answer = Answer(
-            id = answers.size.toLong() + 1,
             message = request.message,
             author = userService.findById(request.authorId),
             topic = topicService.findById(id),
         )
 
-        answers = answers.plus(answer)
-
-        return answer
+        return repository.save(answer)
     }
 
     fun getAnswer(
         id: Long,
-        topicId: Long
     ): Answer {
-        return answers.find {
-            it.id == id && it.topic.id == topicId
-        } ?: throw NotFoundException(notFoundErrorMessage.format(id))
+        return repository.findById(id)
+            .orElseThrow { NotFoundException(notFoundErrorMessage.format(id)) }
     }
 
-    fun update(request: UpdateAnswerRequest, topicId: Long): Answer {
-        val answer = getAnswer(request.id, topicId)
-        val updatedAnswer = answer.copy(message = request.message)
+    fun update(request: UpdateAnswerRequest): Answer {
+        val answer = getAnswer(request.id)
+        answer.message = request.message
 
-        answers = answers.minus(answer).plus(updatedAnswer)
-
-        return updatedAnswer
+        return answer
     }
 
-    fun delete(id: Long, topicId: Long) {
-        val answer = getAnswer(id, topicId)
-        answers = answers.minus(answer)
-    }
-
+    fun delete(id: Long) = repository.deleteById(id)
 }
